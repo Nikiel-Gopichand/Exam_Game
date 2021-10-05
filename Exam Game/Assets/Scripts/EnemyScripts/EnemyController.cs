f8970e59b51f1cab7be3a1a49f1d58fcc0c61a5b
KushKushStudent <1882078@students.wits.ac.za>
2021-10-05 12:27:17 +0200

    1

--- Exam Game/Assets/Scripts/EnemyScripts/EnemyController.cs
+++ Exam Game/Assets/Scripts/EnemyScripts/EnemyController.cs
@@ -23,7 +23,15 @@ public class EnemyController : MonoBehaviour
         if (distance<= sightRadius) 
         {
             agent.SetDestination(target.position);
+            if (distance<=agent.stoppingDistance) {
+                FaceTarget();
+            }
         }
+    }
+    void FaceTarget() {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+    transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
     }
     private void OnDrawGizmosSelected()
     {
