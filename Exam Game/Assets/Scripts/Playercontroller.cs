0b38267da309e7fd65773e52c08bcedf45de7ab5
KushKushStudent <1882078@students.wits.ac.za>
2021-10-08 18:41:49 +0200

    1

--- Exam Game/Assets/Scripts/Playercontroller.cs
+++ Exam Game/Assets/Scripts/Playercontroller.cs
@@ -9,11 +9,12 @@ public class Playercontroller : MonoBehaviour
     public GameObject camPivot;
     public Vector2 turn;
     public float sensitivity = 2f;
+    public Collider sword;
     Vector3 runV;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sword.enabled = false;
     }
 
     // Update is called once per frame
@@ -24,21 +25,30 @@ public class Playercontroller : MonoBehaviour
         runV = camPivot.transform.forward * Input.GetAxis("Vertical") + camPivot.transform.right * Input.GetAxis("Horizontal");
         runV.y = 0f;
         this.GetComponent<Rigidbody>().velocity = runV*speed;
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
+            sword.enabled = true;
             anim.SetInteger("atk", (anim.GetInteger("atk") == 0 ? 1 : anim.GetInteger("atk") == 1 ? 2 : 0));
             anim.SetTrigger("attack");
+            StartCoroutine("swordAttack");
+           
         }
         if (GetComponent<Rigidbody>().velocity.magnitude != 0)
         {
             anim.SetFloat("Running",1);
             Vector3 lDir = transform.position + runV;
-            this.transform.forward = Vector3.Lerp(transform.forward, runV, 20f * Time.deltaTime);
+            this.transform.forward = Vector3.Lerp(transform.forward, runV, 40f * Time.deltaTime);
            
         }
         if (GetComponent<Rigidbody>().velocity.magnitude == 0)
         {
             anim.SetFloat("Running", 0);
         }
+    }
+
+    public IEnumerator swordAttack() {
+
+        yield return new WaitForSeconds(1);
+        sword.enabled = false;
     }
 }
