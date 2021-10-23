ff4d1b66fe3f3d3618c884a0a923c655bcde6b17
KushKushStudent <1882078@students.wits.ac.za>
2021-10-23 09:45:02 +0200

    1

---
+++ Exam Game/Assets/Scripts/Quests/QuestGiver.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestGiver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public QuestController quest;
+    public Playercontroller player;
+}
